1c44b749a3fddfaf01b1c765e972c0c5cbfb1511
joelnunez19 <joelnunez855@gmail.com>
2025-02-10 12:55:32 -0300

    ss

---
+++ cypress/e2e/pages/login/prueba.cy.js
@@ -0,0 +1,3 @@
+it('TC1 | Emitir solicitud con datos completos',()=>{
+    cy. visit('https://www.google.com.ar/')
+    })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -11,5 +11,5 @@
 //
 // You can read more here:
 // https://on.cypress.io/configuration
-import '@shelex/cypress-allure-plugin';
+import '@mmisty/cypress-allure-adapter/support';
 import './commands'
