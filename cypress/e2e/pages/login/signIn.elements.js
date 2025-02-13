afa85b3b7b0a1808743af606cc66a54c8346b25e
joelnunez19 <joelnunez855@gmail.com>
2025-02-13 11:59:33 -0300

    "Sig in page"

---
+++ cypress/e2e/pages/login/signIn.elements.js
@@ -0,0 +1,28 @@
+export class LoginElements_SignIn {
+    static get navbar_SigIn(){
+        return{
+            get btnSigIn(){
+                return cy.get('#signin2')
+            }
+        }
+    }
+    static get textboxes(){
+        return{
+            get username(){
+                return cy.get('input[type="text"]#sign-username').click();
+            },
+            get password(){
+                return cy.get('input[type="password"]#sign-password').click()
+            }
+        }
+    }
+    static get button(){
+       return {
+        get signUp(){
+            return cy.get('button[type="button"][onclick="register()"]');
+        },
+        get close(){
+            return cy.contains('button','Close').should('be.visible');
+        }
+    }}
+}
