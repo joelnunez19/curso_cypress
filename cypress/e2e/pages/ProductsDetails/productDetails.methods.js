9b221aa21cff6177efaf7ec30c8e4458995ce8ef
joelnunez19 <joelnunez855@gmail.com>
2025-03-06 15:55:02 -0300

    se creo Product details page

---
+++ cypress/e2e/pages/ProductsDetails/productDetails.methods.js
@@ -0,0 +1,7 @@
+import { productDetailsElements } from "./ProductDetails.elements";
+
+export class productDetailsMethods{
+    static clickOnAddToCart(){
+        productDetailsElements.buttons.addToCart.click()
+    }
+}
